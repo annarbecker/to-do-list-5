2d02218f5b076f227ea76b06a8f78ea2167149d4
Anna Becker and Kyle Derrick <kderrick1988@gmail.com>
2016-02-23 08:24:13 -0800

    add initial files

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import org.junit.ClassRule;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+}
